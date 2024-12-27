6a1d97babdbcca8ff9d21cad43197b4249c152a7
evanosouth <evanosouth@gmail.com>
2024-12-27 12:43:48 +0000

    pertemuan terakhir

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\barangKeluar;
+use App\Models\pelanggan;
+use App\Models\stok;
+use App\Models\suplier;
 use Illuminate\Http\Request;
 
 class dashboardController extends Controller
 {
     public function index(){
-        return view(
-            'Dashboard.dashboard'
-        );
+
+        $getSuplier = suplier::count();
+        $getPelanggan = pelanggan::count();
+        $getStok = stok::count();
+        $getPendapatan = barangKeluar::sum('sub_total');
+        return view('Dashboard.dashboard', compact(
+            'getSuplier',
+            'getPelanggan',
+            'getStok',
+            'getPendapatan',
+        ));
     }
 }
